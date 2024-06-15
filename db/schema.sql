5fb26fab1d650dbd0953198d19ac605829c02eee
StivenF0 <stivenfonseca201@gmail.com>
2024-06-15 13:54:57 -0300

    Include user_id on schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS history (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id VARCHAR(36) NOT NULL,
     datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     initial_video TEXT NOT NULL,
-    second_video TEXT NOT NULL
+    second_video TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
