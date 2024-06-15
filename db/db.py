75de61a653437e96876b7d5dad5bca0525565c24
StivenF0 <stivenfonseca201@gmail.com>
2024-06-15 17:28:57 -0300

    Fix bugs on code structure

--- db/db.py
+++ db/db.py
@@ -1,13 +1,15 @@
 from uuid import uuid4
+from os.path import join as path_join
 import sqlite3
 
 
 DB_NAME = "database.db"
-DB = "sqlite:///" + DB_NAME
+DB_DIR = "db/"
+
 
 class Database:
-    def __init__(self) -> None:
-        self.connection = sqlite3.connect(DB)
+    def __init__(self, cwd) -> None:
+        self.connection = sqlite3.connect(path_join(cwd, DB_DIR, DB_NAME))
         self.cursor = self.connection.cursor()
     
     def close(self) -> None:
@@ -15,7 +17,7 @@ class Database:
         self.connection.close()
 
     def register_user(self, name: str, password_hash: str) -> None:
-        uuid = uuid4()
+        uuid = str(uuid4())
         self.cursor.execute("INSERT INTO users(id, name, password_hash) VALUES (?, ?, ?)", 
                             (uuid, name, password_hash))
         self.connection.commit()
