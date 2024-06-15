a0a72bff9c7b99af71accbe63de2abf43693536c
StivenF0 <stivenfonseca201@gmail.com>
2024-06-15 17:11:49 -0300

    Add dependencies and functionality for register

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect
+from helpers import show_error
+from db.db import Database
 
 app = Flask(__name__)
 
@@ -19,9 +22,31 @@ def history():
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return show_error("Invalid username")
+        if not password:
+            return show_error("Missing password")
+        if not confirmation:
+            return show_error("Missing confirmation of password")
+        if confirmation != password:
+            return show_error("Passwords do not match")
+        
+        hash = generate_password_hash(password)
+
+        db = Database()
+        db.register_user(username, hash)
+        db.close()
+
+        return redirect("index")
+    else:
+        return render_template("register.html")
 
 
 if __name__ == "__main__":
