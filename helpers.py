f116147419575ed729bd33ec20d0d52330b7dac9
StivenF0 <stivenfonseca201@gmail.com>
2024-06-17 10:32:33 -0300

    Add function that checks if user is logged in

--- helpers.py
+++ helpers.py
@@ -1,4 +1,13 @@
-from flask import render_template
+from flask import render_template, session, redirect
+from functools import wraps
 
 def show_error(msg: str):
     return render_template("error.html", message=msg)
+
+def login_required(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return wrapped
